20515a8f1990fb7c09062f12ef6bbd95fc8faafb
garvit arora <garvit2205.be21@chitkara.edu.in>
2024-09-15 16:55:25 +0530

    suryajii

--- fruit_faq_api/settings.py
+++ fruit_faq_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 # import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    
      'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -132,7 +135,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # MEDIA_URL='media/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
